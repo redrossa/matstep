182349eefdc450d6f757008e3966655ebf17233f
redrossa <adriano.raksi@gmail.com>
2020-11-30 20:13:07 -0600

    Initial commit

--- linpy/solver.py
+++ linpy/solver.py
@@ -1,28 +1,30 @@
-class Relation:
-    def __init__(self, *args, **kwargs):
-        pass
+from linpy.util import eq
 
 
 class Solver:
     def __init__(self, *args, **kwargs):
-        initial_exp = None
-        relation = None
-        final_exp = None
+        exp = None
+        rel = None
+        sol = None
         is_exp = False
 
         if len(args) == 1:
-            initial_exp = args[0]
+            exp = args[0]
             is_exp = True
+        elif len(args) == 2:
+            exp = args[0]
+            rel = eq
+            sol = args[2]
         elif len(args) == 3:
-            initial_exp = args[0]
-            relation = args[1]
-            final_exp = args[2]
+            exp = args[0]
+            rel = args[1]
+            sol = args[2]
 
-        if initial_exp is None:
+        if exp is None:
             raise ValueError('Invalid argument')
 
-        self.initial_exp = initial_exp
-        self.relation = relation
-        self.final_exp = final_exp
+        self.exp = exp
+        self.rel = rel
+        self.sol = sol
         self.is_exp = is_exp
 
