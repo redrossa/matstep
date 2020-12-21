37eb85590a86711468cc2d0b58609ee7e85b2a1f
Reno Raksi <adriano.raksi@gmail.com>
2020-12-21 01:20:14 -0600

    Implemented get_steps and created a private, module-level simplifier

--- matstep/__init__.py
+++ matstep/__init__.py
@@ -1,19 +1,44 @@
-from matstep.simplifiers import StepSimplifier
+import matstep.simplifiers
+
+
+_simplifier = simplifiers.StepSimplifier()
 
 
 def simplify_next(expr, simplifier=None):
-    simplifier = StepSimplifier() if not simplifier else simplifier
+    simplifier = _simplifier if not simplifier else simplifier
     return simplifier(expr)
 
 
 def simplify_full(expr, simplifier=None):
-    simplifier = StepSimplifier() if not simplifier else simplifier
+    simplifier = _simplifier if not simplifier else simplifier
     last_step = simplifier(expr)
 
     while True:
         curr_step = simplifier(last_step)
-        if curr_step == last_step:
-            break
+        try:
+            if curr_step == last_step:
+                break
+        except Exception:
+            pass
         last_step = curr_step
 
     return curr_step
+
+
+def get_steps(expr, simplifier=None):
+    simplifier = _simplifier if not simplifier else simplifier
+    steps = [expr]
+    last_step = simplifier(expr)
+
+    while True:
+        curr_step = simplifier(last_step)
+        try:
+            if curr_step == last_step:
+                break
+        except Exception:
+            pass
+        steps.append(last_step)
+        last_step = curr_step
+    steps.append(curr_step)
+
+    return steps
