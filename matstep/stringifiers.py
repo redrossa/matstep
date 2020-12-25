5128a703753e3f734ce8c9ff240b200b0883eecb
Reno Raksi <adriano.raksi@gmail.com>
2020-12-25 10:49:24 -0600

    Overrides mapping methods for tuples, lists, and numpy arrays

--- matstep/stringifiers.py
+++ matstep/stringifiers.py
@@ -11,6 +11,15 @@ class StepStringifier(StringifyMapper):
     of the returned value of a call to a `StepSimplifier`.
     """
 
+    def map_tuple(self, expr, enclosing_prec, *args, **kwargs):
+        return '(' + ', '.join(self.rec(el, enclosing_prec, *args, **kwargs) for el in expr) + ')'
+
+    def map_list(self, expr, enclosing_prec, *args, **kwargs):
+        return '[' + ', '.join(self.rec(el, enclosing_prec, *args, **kwargs) for el in expr) + ']'
+
+    def map_numpy_array(self, expr, enclosing_prec, *args, **kwargs):
+        return self.map_list(expr, enclosing_prec, *args, **kwargs)
+
     def map_foreign(self, expr, *args, **kwargs):
         try:
             return super(StepStringifier, self).map_foreign(expr, *args, **kwargs)
