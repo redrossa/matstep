6d55c491538b1fd06d470667500d3fab3d8bff25
Reno Raksi <adriano.raksi@gmail.com>
2021-01-27 16:32:40 -0600

    Added an example for the creation of logical expressions and truth tables

---
+++ examples/truth_table.py
@@ -0,0 +1,22 @@
+"""
+Demonstrates the creation of logical expressions and truth tables.
+
+`matstep.logic.Proposition` represents the 'variable' in a logical
+expression. The built-in logical operators that can be performed on
+a `Proposition` are:
+  not (~p)
+  and (p & q)
+  inclusive or (p | q)
+  implication (p >> q) !!! This must be surrounded by parentheses as
+  necessary since python (>>) operator has a higher precedence than
+  any of the above.
+"""
+
+from matstep.logic import Proposition, tabulate
+
+
+p, q, r = Proposition('p'), Proposition('q'), Proposition('r')
+e = (p | ~(q & ~r)) >> (p & r)
+t = tabulate(e)
+
+print(t.to_markdown())
